e7153d92a55ac6ceec939dc0aae621af7bf60bfa
mehdi <mehdi.kerbedj@wescale.fr>
2020-07-15 15:00:05 +0200

    update test

--- example/main.tf
+++ example/main.tf
@@ -1,42 +1,73 @@
 provider "aws" {
   region = "eu-west-1"
 }
+
 module "sg" {
-  source         = "../"
-  name           = "first test"
-  sg_description = " sg test"
-  vpc_id         = "vpc-094669585248286af"
-  tags           = {Name="sg test",Test ="it works"}
+  source         = "mehdi-wsc/sg-wsc/aws"
+  version        = "0.0.1"
+  name           = "firsttest"
+  vpc_id         = "vpc-06733116ce9a073c7" 
+  sg_description = " test "
   ingress_rules = [
     {
       from_port   = 8080
       to_port     = 8080
       protocol    = "tcp"
-      description = "User-service ports"
-      cidr_blocks = "10.10.0.0/16"
+      cidr_blocks = "0.0.0.0/0"
 
-    },
+    },   
     {
-      from_port   = 22
-      to_port     = 22
+      from_port   = 80
+      to_port     = 80
       protocol    = "tcp"
-      description = "ssh"
-      cidr_blocks = "10.10.0.0/16"
+      cidr_blocks = "0.0.0.0/0"
 
     }
   ]
   egress_rules = [
-          {
-      description = "ssh"
-      cidr_blocks = "10.10.0.0/16"
+    {
+      cidr_blocks = "0.0.0.0/0"
 
     }
   ]
 }
+data "aws_ami" "wp" {
+  most_recent = true
 
+  filter {
+    name   = "name"
+    values = ["bitnami-wordpress-4.7.3-1-linux-debian-8-x86_64-frontend-rds-nami-hvm-ebs"]
+
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["979382823631"]
+}
+
+
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.wp.id
+  instance_type = "t2.micro"
+  security_groups = ["${module.sg.sg_id}"]
+  subnet_id = "subnet-0e4f937c40f6ed4a5"
+  associate_public_ip_address = true
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF  
+}
 output "sg" {
   value = module.sg.sg_id
 }
 output "arn" {
-  value = module.sg.
+  value = module.sg.sg_arn
+}
+
+output "web" {
+  value = "http://${aws_instance.web.public_ip}:80"
 }
---
+++ test/Sg_test.go
@@ -0,0 +1,32 @@
+package test
+
+import (
+	"net/http"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/gruntwork-io/terratest/modules/terraform"
+)
+
+func TestIntegrationAsg(t *testing.T) {
+	terraformOptions := &terraform.Options{
+		TerraformDir: "../example",
+	}
+
+	defer terraform.Destroy(t, terraformOptions)
+	terraform.InitAndApply(t, terraformOptions)
+	duration := time.Duration(200) * time.Second
+	time.Sleep(duration)
+	url := terraform.Output(t, terraformOptions, "web")
+
+	resp, err := http.Get(url)
+	if err != nil {
+		os.Exit(1)
+
+	}
+	if resp == nil {
+		os.Exit(2)
+	}
+
+}
